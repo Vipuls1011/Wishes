960a6f2fc647638c1b4ba7fe60cba11a59e9c337
Vipuls1011 <159099795+Vipuls1011@users.noreply.github.com>
2025-01-07 18:18:30 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,9 +1,6 @@
 <?php
-
-$myfile = fopen("what.txt", "w")
-
+$myfile = fopen("what.txt", "w");
 $txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"];
 fwrite($myfile, $txt);
 fclose($myfile);
-
 ?>
